273e547920841ff637b0979c454da1282b5a71f2
pedroferreira <pmacf@iscte-iul.pt>
2024-05-14 23:53:19 +0100

    game over text added

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -8,7 +8,7 @@ public class Collision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     playerScore = FindObjectOfType<PlayerScore>();    
+     playerScore = FindObjectOfType<PlayerScore>();        
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerScore : MonoBehaviour
 {
+    public Image gameOverText;
     public int score;
-    public Rigidbody2D player;
+    private Rigidbody2D player;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +24,16 @@ public class PlayerScore : MonoBehaviour
     {
         // Reduzir os pontos do jogador
         if(score - 1 <= 0) {
-            Destroy(player);
+            gameOverText.gameObject.SetActive(true); // Activate the "Game Over" text
+            RectTransform rectTransform = gameOverText.GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            rectTransform.pivot = new Vector2(0.5f, 0.5f);
+            rectTransform.anchoredPosition = Vector2.zero;
+        } else {
+            score -= 1;
         }
-        score -= 1; // Exemplo de redução de 10 pontos, você pode ajustar conforme necessário
+         // Exemplo de redução de 10 pontos, você pode ajustar conforme necessário
         Debug.Log("Player Score: " + score);
     }
 }
